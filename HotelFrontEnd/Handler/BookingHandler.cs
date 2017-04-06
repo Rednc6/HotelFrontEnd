3d84fee2b551521b9f8accee878405ca4a04b60a
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-04-06 14:56:12 +0200

    Booking handler created bookingVM updated

---
+++ HotelFrontEnd/Handler/BookingHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelFrontEnd.ViewModel;
+
+namespace HotelFrontEnd.Handler
+{
+    class BookingHandler
+    {
+        public BookingViewModel BookingViewModel { get; set; }
+
+        public BookingHandler(BookingViewModel vm)
+        {
+            this.BookingViewModel = vm;
+        }
+
+        public void AddBooking()
+        {
+ //           Booking newBooking = new Booking()
+        }
+    } 
+}
--- HotelFrontEnd/ViewModel/BookingViewModel.cs
+++ HotelFrontEnd/ViewModel/BookingViewModel.cs
@@ -6,17 +6,67 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using HotelFrontEnd.Commen;
+using HotelFrontEnd.Persistency;
 
 namespace HotelFrontEnd.ViewModel
 {
     class BookingViewModel : INotifyPropertyChanged
     {
+        //RelayCommands
+        public ICommand AddBookingCommand { get; set; }
         public Singleton GuestSingl { get; set; }
+
+        private int _bookingID;
+        public int BookingID
+        {
+            get { return _bookingID; }
+            set { _bookingID = value; }
+        }
+
+        private int _guestID;
+        public int GuestID
+        {
+            get { return _guestID; }
+            set { _guestID = value;
+                OnPropertyChanged(nameof(GuestID));
+            }
+        }
+
+        private int _roomID;
+        public int RoomID
+        {
+            get { return _roomID; }
+            set { _roomID = value;
+                OnPropertyChanged(nameof(RoomID));
+            }
+        }
+
+        private DateTimeOffset _dateTo;
+        public DateTimeOffset DateTo
+        {
+            get { return _dateTo; }
+            set { _dateTo = value;
+                OnPropertyChanged(nameof(DateTo));
+            }
+        }
+
+        private DateTimeOffset _dateFrom;
+        public DateTimeOffset DateFrom
+        {
+            get { return _dateFrom; }
+            set { _dateFrom = value;
+                OnPropertyChanged(nameof(DateFrom));
+            }
+        }
+
 
         //CTOR
         public BookingViewModel()
         {
             GuestSingl = Singleton.Instance;
+//            AddBookingCommand = new RelayCommand();
         }
 
 
