47028b35fc2c771dc0d7230d11a0826ada012604
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-04-06 11:58:42 +0200

    Added converter til datetime Opsat skelet til booking

---
+++ HotelFrontEnd/ViewModel/BookingViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HotelFrontEnd.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFrontEnd.ViewModel
+{
+    class BookingViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Guest> _guestCollection;
+
+        public ObservableCollection<Guest> GuestCollection
+        {
+            get { return _guestCollection; }
+            set { _guestCollection = value; }
+        }
+
+
+
+        //CTOR
+        public BookingViewModel()
+        {
+
+        }
+
+        //PropetyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
