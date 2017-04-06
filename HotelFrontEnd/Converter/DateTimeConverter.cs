d743bab6a9c6935deb69cd823279ab582fa65944
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-04-06 13:39:12 +0200

    Model class booking done

--- HotelFrontEnd/Converter/DateTimeConverter.cs
+++ HotelFrontEnd/Converter/DateTimeConverter.cs
@@ -8,12 +8,12 @@ namespace HotelFrontEnd.Converter
 {
     class DateTimeConverter
     {
-        public static DateTime DateTimeArrive(DateTimeOffset date, TimeSpan time)
+        public static DateTime DateTimeArrive(DateTimeOffset date)
         {
             return new DateTime(date.Year, date.Month, date.Day, 14, 0, 0);
         }
 
-        public static DateTime DateTimeLeave(DateTimeOffset date, TimeSpan time)
+        public static DateTime DateTimeLeave(DateTimeOffset date)
         {
             return new DateTime(date.Year, date.Month, date.Day, 12, 0, 0);
         }
--- HotelFrontEnd/Model/Booking.cs
+++ HotelFrontEnd/Model/Booking.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelFrontEnd.Converter;
 
 namespace HotelFrontEnd.Model
 {
     class Booking
     {
+        public int Guest_ID { get; set; }
+        public int Room_ID { get; set; }
+        public DateTime Date_From { get; set; }
+        public DateTime Date_To { get; set; }
+
+        //CTOR
+        public Booking(int GuestID, int RoomID, DateTimeOffset DateFrom, DateTimeOffset DateTo)
+        {
+            this.Guest_ID = GuestID;
+            this.Room_ID = RoomID;
+            this.Date_From = DateTimeConverter.DateTimeArrive(DateFrom);
+            this.Date_To = DateTimeConverter.DateTimeLeave(DateTo);
+        }
+
+        public override string ToString()
+        {
+            return $"Guest: {Guest_ID} Room: {Room_ID}";
+        }
+
+
     }
 }
